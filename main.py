5c4637561970496fd6053899176af628cb4abd25
Matéo SALVADOR <salvador.mateo205@gmail.com>
2026-04-17 19:43:00 +0200

    all pieces just added and board fixed

--- main.py
+++ main.py
@@ -1,28 +1,127 @@
 from rich.console import Console
 from rich.text import Text
-
+from abc import ABC
 console = Console(force_terminal=True, color_system="256")
 
-class Pion():
-    def __init__(self):
-        self.display = "♝"
+class GameManager():
+    pass
+
+
+class Player():
+    pass
+
+class Pawn(ABC):
+    def __init__(self, Owner):
+        self.display = "   "
+        self.owner = Owner
+        self.name = ""
 
     def return_display(self):
         return self.display
+    
+class WhitePawn(Pawn):
+    def __init__(self, Owner: Player):
+        super().__init__(Owner)
+        self.display = "♙"
+        self.color = "white"
+        self.name = "White Pawn"
+
+class WhiteQueen(WhitePawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♕"
+        self.color = "white"
+        self.name = "White Queen"
+
+class WhiteKing(WhitePawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♔"
+        self.color = "white"
+        self.name = "White King"
+
+class WhiteRook(WhitePawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♖"
+        self.color = "white"
+        self.name = "White Rook"
+
+class WhiteBishop(WhitePawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♗"
+        self.color = "white"
+        self.name = "White Bishop"
+
+class WhiteKnight(WhitePawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♘"
+        self.color = "white"
+        self.name = "Knight"
+
+class BlackPawn(Pawn):
+    def __init__(self, Owner):
+        self.display = "♟"
+        self.owner = Owner
+        self.color = "black"
+
+class BlackQueen(BlackPawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♛"
+        self.color = "black"
+        self.name = "Black Queen"
+
+class BlackKing(BlackPawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♔"
+        self.color = "black"
+        self.name = "Black King"
+
+class BlackRook(BlackPawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♖"
+        self.color = "black"
+        self.name = "Black Rook"
+
+class BlackBishop(BlackPawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♗"
+        self.color = "black"
+        self.name = "Black Bishop"
+
+class BlackKnight(BlackPawn):
+    def __init__(self, Owner):
+        super().__init__(Owner)
+        self.display = "♘"
+        self.color = "black"
+        self.name = "Knight"
 
 class Case():
     def __init__(self, color, state):
         self.color = color
         self.state = state
 
-    def return_display(self):
-        content = f" {self.state.return_display()} " if self.state else "   "
-        bg = "grey23" if self.color == "black" else "grey84"
-        t = Text(content)
-        t.stylize(f"on {bg}")
-        return t
+    def change_state(self, new_state):
+        self.state = new_state
 
-class Ligne():
+    def return_display(self):
+        bg = "grey23" if self.color == "black" else "grey84"
+        if self.state:
+            t = Text(f" {self.state.display} ")
+            t.stylize(f"{self.state.color} on {bg}")
+            return t
+        else:
+            t = Text("   ")
+            t.stylize(f"on {bg}")
+            return t
+
+class Lane():
     def __init__(self, row):
         self.line = []
         self.row = row
@@ -31,7 +130,7 @@ class Ligne():
     def fill_raw(self):
         if self.row % 2 == 0:
             for i in range(4):
-                self.line.append(Case("white", Pion()))
+                self.line.append(Case("white", None))
                 self.line.append(Case("black", None))
         else:
             for i in range(4):
@@ -44,13 +143,41 @@ class Ligne():
             result.append_text(case.return_display())
         return result
 
-class Plateau():
+class Board():
     def __init__(self):
-        self.plateau = [Ligne(i) for i in range(8)]
+        self.board = [Lane(i) for i in range(8)]
+
+    def config_board(self, Player1: Player, Player2: Player):
+        for case in self.board[6].line:
+            case.change_state(WhitePawn(Player1))
+        self.board[7].line[0].change_state(WhiteRook(Player1))
+        self.board[7].line[7].change_state(WhiteRook(Player1))
+        self.board[7].line[1].change_state(WhiteKnight(Player1))
+        self.board[7].line[6].change_state(WhiteKnight(Player1))
+        self.board[7].line[2].change_state(WhiteBishop(Player1))
+        self.board[7].line[5].change_state(WhiteBishop(Player1))
+        self.board[7].line[3].change_state(WhiteQueen(Player1))
+        self.board[7].line[4].change_state(WhiteKing(Player1))
+
+        for case in self.board[1].line:
+            case.change_state(BlackPawn(Player2))
+        self.board[0].line[0].change_state(BlackRook(Player2))
+        self.board[0].line[7].change_state(BlackRook(Player2))
+        self.board[0].line[1].change_state(BlackKnight(Player2))
+        self.board[0].line[6].change_state(BlackKnight(Player2))
+        self.board[0].line[2].change_state(BlackBishop(Player2))
+        self.board[0].line[5].change_state(BlackBishop(Player2))
+        self.board[0].line[3].change_state(BlackQueen(Player2))
+        self.board[0].line[4].change_state(BlackKing(Player2))
 
     def display_plateau(self):
-        for ligne in self.plateau:
-            console.print(ligne.display_line())
+        for lane in self.board:
+            console.print(lane.display_line())
+            
 
-plateau = Plateau()
+
+Player1 = Player()
+Player2 = Player()
+plateau = Board()
+plateau.config_board(Player1, Player2)
 plateau.display_plateau()
